cf35b2f307f540dd9575907f69b1b6b68cad6593
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2017-03-07 07:41:20 +0300

    Точка входа для билдеров

---
+++ src/main/java/ru/d_shap/hash/builder/HashBuilder.java
@@ -0,0 +1,128 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Hash library provides facilities for the hash computations.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of Hash library.
+//
+// Hash library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Hash library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.hash.builder;
+
+import java.io.InputStream;
+
+/**
+ * Hash builder entry point.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class HashBuilder {
+
+    private HashBuilder() {
+        super();
+    }
+
+    /**
+     * Create the simple hash builder.
+     *
+     * @param bytes the byte array.
+     * @return the simple hash builder.
+     */
+    public static SimpleHashBuilder newSimpleHashBuilder(final byte[] bytes) {
+        return new ByteArraySimpleHashBuilder(bytes);
+    }
+
+    /**
+     * Create the simple hash builder.
+     *
+     * @param str      the string.
+     * @param encoding the string encoding.
+     * @return the simple hash builder.
+     */
+    public static SimpleHashBuilder newSimpleHashBuilder(final String str, final String encoding) {
+        return new StringSimpleHashBuilder(str, encoding);
+    }
+
+    /**
+     * Create the simple hash builder.
+     *
+     * @param stream the input stream.
+     * @return the simple hash builder.
+     */
+    public static SimpleHashBuilder newSimpleHashBuilder(final InputStream stream) {
+        return new InputStreamSimpleHashBuilder(stream);
+    }
+
+    /**
+     * Create the hash with the salt builder.
+     *
+     * @param bytes the byte array.
+     * @return the hash with the salt builder.
+     */
+    public static HashWithSaltBuilder newHashWithSaltBuilder(final byte[] bytes) {
+        return new ByteArrayHashWithSaltBuilder(bytes);
+    }
+
+    /**
+     * Create the hash with the salt builder.
+     *
+     * @param str      the string.
+     * @param encoding the string encoding.
+     * @return the hash with the salt builder.
+     */
+    public static HashWithSaltBuilder newHashWithSaltBuilder(final String str, final String encoding) {
+        return new StringHashWithSaltBuilder(str, encoding);
+    }
+
+    /**
+     * Create the hash with the salt builder.
+     *
+     * @param stream the input stream.
+     * @return the hash with the salt builder.
+     */
+    public static HashWithSaltBuilder newHashWithSaltBuilder(final InputStream stream) {
+        return new InputStreamHashWithSaltBuilder(stream);
+    }
+
+    /**
+     * Create the hash with the fixed salt and the stored salt builder.
+     *
+     * @param bytes the byte array.
+     * @return the hash with the fixed salt and the stored salt builder.
+     */
+    public static HashWithSalt2Builder newHashWithSalt2Builder(final byte[] bytes) {
+        return new ByteArrayHashWithSalt2Builder(bytes);
+    }
+
+    /**
+     * Create the hash with the fixed salt and the stored salt builder.
+     *
+     * @param str      the string.
+     * @param encoding the string encoding.
+     * @return the hash with the fixed salt and the stored salt builder.
+     */
+    public static HashWithSalt2Builder newHashWithSalt2Builder(final String str, final String encoding) {
+        return new StringHashWithSalt2Builder(str, encoding);
+    }
+
+    /**
+     * Create the hash with the fixed salt and the stored salt builder.
+     *
+     * @param stream the input stream.
+     * @return the hash with the fixed salt and the stored salt builder.
+     */
+    public static HashWithSalt2Builder newHashWithSalt2Builder(final InputStream stream) {
+        return new InputStreamHashWithSalt2Builder(stream);
+    }
+
+}
