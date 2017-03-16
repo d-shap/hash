6cde8dcf51d485f571d67222bc37f32359a0e9fc
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2017-03-16 07:57:28 +0300

    заглушки для тестов

--- src/test/java/ru/d_shap/hash/builder/ByteArrayHashWithSalt2BuilderTest.java
+++ src/test/java/ru/d_shap/hash/builder/ByteArrayHashWithSalt2BuilderTest.java
@@ -19,6 +19,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 package ru.d_shap.hash.builder;
 
+import org.junit.Test;
+
 /**
  * Tests for {@link ByteArrayHashWithSalt2Builder}.
  *
@@ -31,6 +33,342 @@ public final class ByteArrayHashWithSalt2BuilderTest {
      */
     public ByteArrayHashWithSalt2BuilderTest() {
         super();
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getStoredSaltTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void setStoredSaltTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void storedSaltDifferentTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getFixedSaltTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void setFixedSaltTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void fixedSaltDifferentTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addSaltTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addNullHashSaltFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addNullStoredSaltSaltFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addNullFixedSaltSaltFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addNullSaltOrderSaltFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addStoredSaltTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addNullHashStoredSaltTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addNullStoredSaltStoredSaltTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addNullFixedSaltStoredSaltTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addNullSaltOrderStoredSaltTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addSaltBytesTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addNullHashSaltBytesFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addNullStoredSaltSaltBytesFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void addNullSaltStoreTypeSaltBytesFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getSaltOrderTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void setSaltOrderTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getSaltStoreTypeTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void setSaltStoreTypeTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getStoredSaltLenghtTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void setStoredSaltLenghtTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getHashTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getNullHashFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getNullStoredSaltHashFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getNullFixedSaltHashFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getNullAlgorithmHashFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getWrongAlgorithmHashFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getNullSaltOrderHashFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void getNullSaltStoreTypeHashFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void isHashValidTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void isNullHashValidFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void isNullStoredSaltHashValidFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void isNullFixedSaltHashValidFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void isNullAlgorithmHashValidFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void isWrongAlgorithmHashValidFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void isNullSaltOrderHashValidFailTest() {
+
+    }
+
+    /**
+     * {@link ByteArrayHashWithSalt2Builder} class test.
+     */
+    @Test
+    public void isNullSaltStoreTypeHashValidFailTest() {
+
     }
 
 }
