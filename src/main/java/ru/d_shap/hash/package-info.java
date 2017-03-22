47efbd09c374e12f91205d0d96cdfe00803c120c
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2017-03-22 16:17:39 +0300

    поправил javadoc

--- src/main/java/ru/d_shap/hash/package-info.java
+++ src/main/java/ru/d_shap/hash/package-info.java
@@ -18,6 +18,36 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /**
- * Classes provide facilities for the hash computations.
+ * <p>
+ * Hash library provides facilities for the hash computations.
+ * </p>
+ * <p>
+ * A hash function is any function that can be used to map data of arbitrary size to data of fixed size.
+ * This can be used for assuring integrity of transmitted data. This is also useful in cryptography.
+ * A cryptographic hash function allows one to easily verify that some input data maps to a given hash value,
+ * but if the input data is unknown, it is deliberately difficult to reconstruct it (or equivalent
+ * alternatives) by knowing the stored hash value.
+ * </p>
+ * <p>
+ * Hash library simplify usage of java.security package classes for the hash functions.
+ * </p>
+ * <p>
+ * There are some common use-cases for the hash functions.
+ * </p>
+ * <p>
+ * To check data integrity for transmitted data (for example, some large file) the hash of this file can be
+ * computed and stored along with the file. After this file is obtained, the hash can be computed once again and
+ * if this hash is equal to the stored hash, then the file is not corrupted.
+ * </p>
+ * <pre>{@code
+ * // Obtain hash of the file
+ * File file = ...
+ * byte[] hash = HashBuilder.newSimpleHashBuilder(new FileInputStream(file)).setAlgorithm(HashAlgorithms.MD5).getHash();
+ *
+ * // Check if the file is not corrupted
+ * File file = ...
+ * byte[] storedHash = ...
+ * HashBuilder.newSimpleHashBuilder(new FileInputStream(file)).setAlgorithm(HashAlgorithms.MD5).setStoredHash(storedHash).isHashValid();
+ * }</pre>
  */
 package ru.d_shap.hash;
