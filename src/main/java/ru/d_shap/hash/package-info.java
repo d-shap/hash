d44ee04d53fb5c5f7793dad519cf026c6e6c9888
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2017-03-30 07:15:22 +0300

    Написал javadoc

--- src/main/java/ru/d_shap/hash/package-info.java
+++ src/main/java/ru/d_shap/hash/package-info.java
@@ -42,12 +42,76 @@
  * <pre>{@code
  * // Obtain hash of the file
  * File file = ...
- * byte[] hash = HashBuilder.newSimpleHashBuilder(new FileInputStream(file)).setAlgorithm(HashAlgorithms.MD5).getHash();
+ * byte[] hash = HashBuilder.newSimpleHashBuilder(new FileInputStream(file))
+ *                          .setAlgorithm(HashAlgorithms.MD5)
+ *                          .getHash();
  *
  * // Check if the file is not corrupted
  * File file = ...
  * byte[] storedHash = ...
- * HashBuilder.newSimpleHashBuilder(new FileInputStream(file)).setAlgorithm(HashAlgorithms.MD5).setStoredHash(storedHash).isHashValid();
+ * HashBuilder.newSimpleHashBuilder(new FileInputStream(file))
+ *            .setAlgorithm(HashAlgorithms.MD5)
+ *            .setStoredHash(storedHash)
+ *            .isHashValid();
+ * }</pre>
+ * <p>
+ * To define a password hash the same code can be used. But the result is not secure enough.
+ * There are rainbow tables, that contain a precomputed hashes for the passwords. This tables can be used
+ * to get the clear password. To improve security of the password hash a salt can be used.
+ * </p>
+ * <p>
+ * A salt is used to complicate a password and to make the same clear passwords different. A salt is a public
+ * data. It can be stored as a clear text. A new salt should be generated for each new password.
+ * </p>
+ * <pre>{@code
+ * // Obtain hash of the password
+ * byte[] password = ...
+ * byte[] salt = ...
+ * byte[] hash = HashBuilder.newHashWithSaltBuilder(password)
+ *                          .setSalt(salt)
+ *                          .setAlgorithm(HashAlgorithms.SHA256)
+ *                          .setSaltStoreType(SaltStoreType.AT_THE_BEGINNING)
+ *                          .getHash();
+ *
+ * // Check if the password is valid
+ * byte[] password = ...
+ * byte[] storedHash = ...
+ * HashBuilder.newHashWithSaltBuilder(password)
+ *            .setAlgorithm(HashAlgorithms.SHA256)
+ *            .setSaltStoreType(SaltStoreType.AT_THE_BEGINNING)
+ *            .setStoredHash(storedHash)
+ *            .isHashValid();
+ * }</pre>
+ * <p>
+ * The hash in the example above is secure enough, but the salt and the hash are stored together.
+ * Another salt can be added, that is known to the application only. This salt is fixed, i.e. is not
+ * changed during application existance. But this salt is a peace of information, that is unknown to
+ * someone who has the stored hash.
+ * </p>
+ * <pre>{@code
+ * // Obtain hash of the password
+ * byte[] password = ...
+ * byte[] storedSalt = ...
+ * byte[] fixedSalt = ...
+ * byte[] hash = HashBuilder.newHashWithSalt2Builder(password)
+ *                          .setStoredSalt(storedSalt)
+ *                          .setFixedSalt(fixedSalt)
+ *                          .setAlgorithm(HashAlgorithms.SHA256)
+ *                          .setSaltOrder(SaltOrder.STORED_SALT_FIRST)
+ *                          .setSaltStoreType(SaltStoreType.AT_THE_BEGINNING)
+ *                          .getHash();
+ *
+ * // Check if the password is valid
+ * byte[] password = ...
+ * byte[] fixedSalt = ...
+ * byte[] storedHash = ...
+ * HashBuilder.newHashWithSalt2Builder(password)
+ *            .setFixedSalt(fixedSalt)
+ *            .setAlgorithm(HashAlgorithms.SHA256)
+ *            .setSaltOrder(SaltOrder.STORED_SALT_FIRST)
+ *            .setSaltStoreType(SaltStoreType.AT_THE_END)
+ *            .setStoredHash(storedHash)
+ *            .isHashValid();
  * }</pre>
  */
 package ru.d_shap.hash;
