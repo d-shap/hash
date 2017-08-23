55bd0aec4c197bd5b9341fb7b64f61dbc649dba4
Dmitry@Dmitry-PC <Dmitry@Dmitry-PC>
2017-08-23 06:58:44 +0300

    Добавил тесты для констант с кодировкой

---
+++ src/test/java/ru/d_shap/hash/DefaultEncodingTest.java
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Hash library provides facilities for the hash computations.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of hash library.
+//
+// Hash library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Hash library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.hash;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.Assertions;
+
+/**
+ * Tests for {@link DefaultEncoding}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class DefaultEncodingTest {
+
+    /**
+     * Test class constructor.
+     */
+    public DefaultEncodingTest() {
+        super();
+    }
+
+    /**
+     * {@link DefaultEncoding} class test.
+     */
+    @Test
+    public void constructorTest() {
+        Assertions.assertThat(DefaultEncoding.class).hasOnePrivateConstructor();
+    }
+
+}
